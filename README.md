67d587fd397e5334a39f42cb2d097a364c9fe907
IsmaelLG <ismael.llopart@gmail.com>
2022-05-16 18:22:56 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+s01t07n02 Llopart Gil Ismael
+
+Fonts:
+
+https://www.youtube.com/watch?v=jrMns2c_LDw&t=1147s (ANNOTATIONS)
+
+https://www.arquitecturajava.com/el-concepto-de-java-annotations/ (ANNOTATIONS)
---
+++ src/main/java/edu/n2/exercise_1/SerializableJSON.java
@@ -0,0 +1,13 @@
+package edu.n2.exercise_1;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(CLASS)
+@Target(TYPE)
+public @interface SerializableJSON {
+	String path();
+}
